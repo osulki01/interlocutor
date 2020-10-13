841f6f6b605d11467b58c0a263963b208794ebd1
gg-kieran <kieran@giffgaff.co.uk>
2020-10-13 23:12:01 +0100

    Rename directory so it is clear it applies to python requirements only

--- utility_scripts/generate_pip_requirements.sh
+++ utility_scripts/generate_pip_requirements.sh
@@ -1,1 +1,1 @@
-.venv/bin/pip-compile --output-file requirements/requirements.txt requirements/requirements.in
+.venv/bin/pip-compile --output-file python_requirements/requirements.txt python_requirements/requirements.in
