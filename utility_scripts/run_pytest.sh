36463076c2aa2dea8fce3d645026f256a8b58845
gg-kieran <kieran@giffgaff.co.uk>
2020-11-29 17:31:37 +0000

    Print statement to distinguish unit from integration tests

--- utility_scripts/run_pytest.sh
+++ utility_scripts/run_pytest.sh
@@ -1,5 +1,11 @@
 # Run unit tests first
+echo "******************************************"
+echo "Running unit tests"
+echo "******************************************"
 docker exec recommender_stg /home/docker_user/.local/bin/pytest --verbose -m 'not integration' interlocutor/
 
 # Run integration tests next
+echo "******************************************"
+echo "Running integration tests"
+echo "******************************************"
 docker exec recommender_stg /home/docker_user/.local/bin/pytest --verbose -m 'integration' interlocutor/
